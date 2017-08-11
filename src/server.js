e83ad20564725e67533566a289c6f8b66656bfd8
Alan Russell <ajrussellaudio@gmail.com>
2017-08-11 14:22:49 +0100

    add server connection and action events

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import Server from 'socket.io';
 
-export default function startServer() {
-  const io = new Server().attach(8090);
-  console.log("server running!")
+export default function startServer(store) {
+  const port = 8090;
+  const io = new Server().attach(port);
+  console.log("Server running on Port", port);
+
+  store.subscribe(() => {
+    io.emit("state", store.getState().toJS());
+  })
+
+  io.on("connection", socket => {
+    socket.emit("state", store.getState().toJS());
+    socket.on("action", store.dispatch.bind(store));
+  })
 }
