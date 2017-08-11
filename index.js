d9bbc1b5faa33136272477048183f1b0d6fa7762
Alan Russell <ajrussellaudio@gmail.com>
2017-08-11 14:46:43 +0100

    load seeds into index and action

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ import startServer from './src/server';
 
 export const store = makeStore();
 startServer(store);
+
+var action = [
+  {type: "SET_ENTRIES", entries: require("./entries.json")},
+  {type: "NEXT"}
+]
+
+actions.forEach(action => {
+  store.dispatch(action);
+})
